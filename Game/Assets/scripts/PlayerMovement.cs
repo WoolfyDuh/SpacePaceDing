47323a25892e993072bed486ec54beb18565bed1
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-05-13 13:43:19 +0200

    Update PlayerMovement.cs

--- Game/Assets/scripts/PlayerMovement.cs
+++ Game/Assets/scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementScript : MonoBehaviour
+public class MovementScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
