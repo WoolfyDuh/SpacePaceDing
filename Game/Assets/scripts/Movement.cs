0c2e4ca18b96fc7b2a726c447789d5ff104c03ca
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-05-14 13:47:33 +0200

    Stuff
    Enemy Toegevoegd n shit

--- Game/Assets/scripts/Movement.cs
+++ Game/Assets/scripts/Movement.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+	public float XVelocity;
+	public float YVelocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+		XVelocity = 5;
+		YVelocity = 5;
     }
 
     // Update is called once per frame
@@ -15,4 +18,11 @@ public class Movement : MonoBehaviour
     {
         
     }
+	private void FixedUpdate()
+	{
+		if (Input.GetKey("W"))
+		{
+			transform.Translate(Vector3.up * (YVelocity + 10) * Time.deltaTime);
+		}
+	}
 }
