10c68a288ab936ab5aaf3a1947523a4587d95fcf
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-05-17 09:37:45 +0200

    Update Movement.cs

--- Game/Assets/scripts/Movement.cs
+++ Game/Assets/scripts/Movement.cs
@@ -11,12 +11,12 @@ public class Movement : MonoBehaviour
     Rigidbody2D r2d2;
     // Start is called before the first frame update
     void Start()
-    {
-		Lives = 3;
-        XVelocity = 5;
-        YVelocity = 7;
-        r2d2 = GetComponent<Rigidbody2D>();
-    }
+    {	
+		Lives = 3;	   //DIT IS ONZE LEVENS BOI
+        XVelocity = 5; //DIT IS ONZE ROTATION BOI
+		YVelocity = 7; //DIT IS ONZE MOVEMENT BOI
+		r2d2 = GetComponent<Rigidbody2D>(); //DIT IS ONZE RIGIDBOI
+	}
 
     // Update is called once per frame
     void Update()
@@ -25,6 +25,7 @@ public class Movement : MonoBehaviour
     }
     private void FixedUpdate()
     {
+		//MOVEMENT forward
         if (Input.GetKey("w"))
         {
             r2d2.AddForce(transform.up * (YVelocity * 50) * Time.deltaTime);
@@ -32,22 +33,22 @@ public class Movement : MonoBehaviour
 			Debug.Log(r2d2.velocity);
         }
         if (Input.GetKey("s"))
-        {
-            r2d2.AddForce(transform.up * -(YVelocity * 50) * Time.deltaTime);
+		{//MOVEMENT BACKWARD
+			r2d2.AddForce(transform.up * -(YVelocity * 50) * Time.deltaTime);
         }
         if (Input.GetKey("d"))
-        {
-            transform.Rotate(Vector3.forward * -(YVelocity * 3) * Time.deltaTime);
+		{ //MOVEMENT LEFT
+			transform.Rotate(Vector3.forward * -(YVelocity * 3) * Time.deltaTime);
         }
         if (Input.GetKey("a"))
-        {
-            transform.Rotate(Vector3.forward * (YVelocity * 3) * Time.deltaTime);
+		{   //MOVEMENT RIGHTO
+			transform.Rotate(Vector3.forward * (YVelocity * 3) * Time.deltaTime);
         }
     }
 	private void OnCollisionEnter(Collision collision)
 	{
 		if (collision.collider.tag == "enemy")
-		{
+		{ //DIT HOORT DE SHIT TE STOPPEN ALS DIE EEN ENEMY AANRACKT
 			r2d2.velocity = Vector3.zero;
 			while (Lives > 0)
 			{
