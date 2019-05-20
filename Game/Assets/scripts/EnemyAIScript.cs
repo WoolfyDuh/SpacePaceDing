513338fde59a127f0ddb3ad71e60ac4b8f2cc139
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-05-20 11:41:30 +0200

    slurp slurp slurp slurp
    enemy AI toegevoegd

--- Game/Assets/scripts/EnemyAIScript.cs
+++ Game/Assets/scripts/EnemyAIScript.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class EnemyAIScript : MonoBehaviour
 {
+	private Vector3 startPos;
+	public GameObject Player;
+	private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+		speed = 50;
+		startPos = transform.position;
+		if (!Player)
+		{
+			Player = GameObject.Find("Player");
+		}
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
     }
+	public void OnCollisionEnter(Collision collision)
+	{
+		
+		if (collision.gameObject.name == "Player")
+		{
+
+			transform.position = startPos;
+		}
+	}
 }
