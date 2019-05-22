f42cfdae447cd6effc8a1bdd29a13f40f276ad2f
Jorikos <26204@ma-web.nl>
2019-05-22 15:51:13 +0200

    some stuff

--- Game/Assets/scripts/EnemyAIScript.cs
+++ Game/Assets/scripts/EnemyAIScript.cs
@@ -4,33 +4,34 @@ using UnityEngine;
 
 public class EnemyAIScript : MonoBehaviour
 {
-	private Vector3 startPos;
-	public GameObject Player;
-	private float speed;
+    private Vector3 startPos;
+    private GameObject Player;
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
-		speed = 4;
-		startPos = transform.position;
-		if (!Player)
-		{
-			Player = GameObject.Find("Player");
-		}
+        Player = GameObject.FindGameObjectWithTag("Player");
+        speed = 4;
+        startPos = transform.position;
+        if (!Player)
+        {
+            Player = GameObject.Find("Player");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-		transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
     }
-	public void OnCollisionEnter(Collision collision)
-	{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
 
-			if (collision.gameObject.tag == "Player")
-			{
-				Destroy(collision.gameObject);
-				Destroy(gameObject);
-				Debug.Log("Hit!");
-			}
-		}
-	}
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            Debug.Log("Hit!");
+        }
+    }
+}
