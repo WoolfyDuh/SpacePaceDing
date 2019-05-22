e38762e555801ec820bc563733d65f11af6926cb
WoolfyDuh <14461196+WoolfyDuh@users.noreply.github.com>
2019-05-22 09:26:54 +0200

    added borders to playable area
    bang ding pow pow badoomf

--- Game/Assets/scripts/ASSteroidSpawnScript.cs
+++ Game/Assets/scripts/ASSteroidSpawnScript.cs
@@ -7,10 +7,20 @@ public class ASSteroidSpawnScript : MonoBehaviour
 	 //position = Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
 	public GameObject asteroid;
 	Vector3 position;
+	Quaternion rotation;
+	float w1;
+	float w2;
+	float w3;
+	float w4;
 	// Start is called before the first frame update
 	void Start()
 	{
-		position = new Vector3(Random.Range(1, 100), 0, Random.Range(2,100));
+		w1 = 2;
+		w2 = 4;
+		w3 = 6;
+		w4 = 8;
+		rotation = new Quaternion(w1,w2,w3,w4);
+		position = new Vector3(Random.Range(1, 10), Random.Range(1, 10), Random.Range(2,10));
 	}
 
 	// Update is called once per frame
@@ -18,7 +28,7 @@ public class ASSteroidSpawnScript : MonoBehaviour
 	{
 		if (Input.GetKeyUp(KeyCode.Return))
 		{
-			Instantiate(asteroid,position, Quaternion.identity);
+			Instantiate(asteroid,position,rotation);
 		}
 	}
 }
